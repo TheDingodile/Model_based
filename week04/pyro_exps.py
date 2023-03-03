770f3fed27a127452f9b8e67b5b4b112b8bfda72
TheDingodile <jakob@3esser.dk>
2023-03-03 12:54:50 +0100

    qwe

--- week04/pyro_exps.py
+++ week04/pyro_exps.py
@@ -1,7 +1,19 @@
 import pyro
+import torch
+from pyro.infer import NUTS
+from pyro.infer import MCMC
 
 def model(tt):
     at = pyro.sample("at", pyro.distributions.Normal(12, 10))
     tu = pyro.sample("tu", pyro.distributions.HalfCauchy(10))
     with pyro.plate("data", len(tt)):
         pyro.sample("tt", pyro.distributions.Normal(at, tu), obs=tt)
+
+# Input data for model - must be a PyTorch tensor!
+tt_obs = torch.tensor([13,17,16,32,12,13,28,12,14,18,36,16,16,31])
+# Run inference in Pyro
+nuts_kernel = NUTS(model)
+mcmc = MCMC(nuts_kernel, num_samples=1000, warmup_steps=500, num_chains=1)
+mcmc.run(tt_obs)
+# Show summary of inference results
+mcmc.summary()
